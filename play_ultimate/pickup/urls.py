2f2f808f38400b896ab322262b312dabe4838633
Shem Sedrick <ssedrick1@gmail.com>
2018-04-26 23:15:58 -0400

    Added login and account menu

--- play_ultimate/pickup/urls.py
+++ play_ultimate/pickup/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'games'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('game/<int:game_id', views.detail, name='Game Detail'),
+    path('login', views.login_request, name="Login"),
+    path('game/<int:game_id>', views.detail, name='Game Detail'),
+    path('account', views.detail, name="account"),
 ]
--- play_ultimate/pickup/views.py
+++ play_ultimate/pickup/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
 
 def index(request):
@@ -9,3 +10,21 @@ def index(request):
 
 def detail(request, game_id):
     return HttpResponse("You are looking at game %s" % game_id)
+
+
+def login_request(request):
+    print("login request")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print('username: {0}').format(username)
+        print('password: {0}').format(password)
+        user = authenticate(username, password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            context = {'error': True}
+            return render(request, "pickup/login.html", context)
+    else:
+        return render(request, "pickup/login.html", context)
